28a953011147eb4a335930e8e2c3ee6c11dd986d
Tim Monroe <timonroe12@gmail.com>
2023-11-13 12:59:20 -0500

    updated the logger and news-scrapers packages, other misc changes

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -1,9 +1,7 @@
 import 'dotenv/config';
-import { Logger } from '@soralinks/logger';
 import { News } from '../../index.js';
 (async () => {
     const news = new News();
     const headlines = await news.getHeadlines();
-    const logger = new Logger({ logInfo: true });
-    logger.info(`headlines: ${JSON.stringify(headlines, null, 2)}`);
+    console.log(`headlines: ${JSON.stringify(headlines, null, 2)}`);
 })();
--- tests/functional/index.ts
+++ tests/functional/index.ts
@@ -1,10 +1,8 @@
 import 'dotenv/config';
-import { Logger } from '@soralinks/logger';
 import { News } from '../../index.js';
 
 (async () => {
   const news: News = new News();
   const headlines: string[] = await news.getHeadlines();
-  const logger: Logger = new Logger({ logInfo: true });
-  logger.info(`headlines: ${JSON.stringify(headlines, null, 2)}`);
+  console.log(`headlines: ${JSON.stringify(headlines, null, 2)}`);
 })();
